59d5b533247c5c806bbf6993e8db098f017ac18e
Peter McKasty <pmck25@yahoo.com>
2023-06-26 18:33:47 -0400

    created function to start the terminal application

--- server.js
+++ server.js
@@ -16,3 +16,56 @@ connection.connect((err) => {
     console.log('Connected to the database.');
     startApp();
   });
+
+  // Function to start the application
+function startApp() {
+    inquirer
+      .prompt({
+        name: 'action',
+        type: 'list',
+        message: 'What would you like to do?',
+        choices: [
+          'View all departments',
+          'View all roles',
+          'View all employees',
+          'Add a department',
+          'Add a role',
+          'Add an employee',
+          'Update an employee role',
+          'Exit',
+        ],
+      })
+      .then((answer) => {
+        switch (answer.action) {
+          case 'View all departments':
+            viewAllDepartments();
+            break;
+          case 'View all roles':
+            viewAllRoles();
+            break;
+          case 'View all employees':
+            viewAllEmployees();
+            break;
+          case 'Add a department':
+            addDepartment();
+            break;
+          case 'Add a role':
+            addRole();
+            break;
+          case 'Add an employee':
+            addEmployee();
+            break;
+          case 'Update an employee role':
+            updateEmployeeRole();
+            break;
+          case 'Exit':
+            console.log('Goodbye!');
+            connection.end();
+            break;
+          default:
+            console.log('Invalid action.');
+            startApp();
+            break;
+        }
+      });
+  }
