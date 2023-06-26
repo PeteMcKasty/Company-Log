760f38eba80971b07e7bc530846ef99584c2aef9
Peter McKasty <pmck25@yahoo.com>
2023-06-25 20:14:33 -0400

    added query to view all employees with employee data, including employee ids, first names, last names, job titles, departments, salaries, and managers they report to

--- db/query.sql
+++ db/query.sql
@@ -5,3 +5,17 @@ SELECT * FROM departments;
 SELECT roles.role_id, roles.title, departments.department_name, roles.salary
 FROM roles
 INNER JOIN departments ON roles.department_id = departments.department_id;
+
+-- Query to view all employees with employee data, including employee ids, first names, last names, job titles, departments, salaries, and managers they report to
+SELECT
+  employees.employee_id,
+  employees.first_name,
+  employees.last_name,
+  roles.title,
+  departments.department_name,
+  roles.salary,
+  CONCAT(managers.first_name, ' ', managers.last_name) AS manager
+FROM employees
+INNER JOIN roles ON employees.role_id = roles.role_id
+INNER JOIN departments ON roles.department_id = departments.department_id
+LEFT JOIN employees AS managers ON employees.manager_id = managers.employee_id;
